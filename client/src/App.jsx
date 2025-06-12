fbe868ce82719105e6cb935246cd635623ec4fee
wadood <wadood2003@gmail.com>
2025-06-12 16:01:37 +0500

    temp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -57,7 +57,7 @@ const newTheme = extendTheme({
   },
 });
 
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
 function App() {
